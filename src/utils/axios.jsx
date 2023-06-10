47421b198e6008f1e0d1db30010625546b0e495a
chandan-002 <chandan.unicoglobal@gmail.com>
2023-06-10 12:39:11 +0530

    backend url added

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 // Setting up base Url for fetching data
 const Axios = axios.create({
-    baseURL: "http://localhost:9000/api/v1/",
+    // baseURL: "http://localhost:9000/api/v1/",
+    baseURL: "https://ideamagix-backend.onrender.com/api/v1/",
     headers: {
         "Content-Type": "application/json",
         "Accept": "application/json",
